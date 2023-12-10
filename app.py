7c19a08745970e394a15ba31d4420341087e2d8f
Bot1noke <alexsys006.ivanov@gmail.com>
2023-12-10 20:36:13 +0300

    aboba

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pandas as pd
-
 df = pd.read_csv("steam.csv")
 hd = df[:20].values.tolist()
 col_names = df.columns
@@ -15,9 +14,15 @@ posts = [
 
 ]
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('main.html', head=col_names, rows=hd)
+    if request.method == 'POST':
+        start = int(request.form['start'])
+        end = int(request.form['end'])
+        df2 = pd.read_csv("steam.csv")
+        mean_price = df2[start:end]['price'].mean()
+        return render_template("result.html", result=mean_price)
+    return render_template('main.html', head=col_names, rows=hd, result='')
 
 @app.route('/Plots')
 def index2():
