3080d053559e42a11640193ff2740e4d8e8a88c8
Bot1noke <alexsys006.ivanov@gmail.com>
2023-12-10 18:34:58 +0300

    aboba

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 import pandas as pd
-import streamlit as st
+#import streamlit as st
 
 df = pd.read_csv("steam.csv")
 hd = df[:20].values.tolist()
@@ -20,21 +20,9 @@ posts = [
 def index():
     return render_template('main.html', head=col_names, rows=hd)
 
-@app.route('/Anal')
+@app.route('/Streamlit')
 def index2():
-    return render_template('main_anal.html', head=col_names, rows=hd)
-
-@app.route('/dora1')
-def index3():
-    return render_template('dora1.html', head=col_names, rows=hd)
-
-@app.route('/dora2')
-def index4():
-    return render_template('dora2.html', head=col_names, rows=hd)
-
-@app.route('/dora3')
-def index5():
-    return render_template('dora3.html', head=col_names, rows=hd)
+    return render_template('streamlit.html', head=col_names, rows=hd)
 
 if __name__ == '__main__':
     app.run(debug=True)
